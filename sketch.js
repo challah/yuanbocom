99db191b260d29c42f47a7cb314d13dadff6f911
challah <yang.bread@gmail.com>
2021-11-17 14:52:33 -0500

    initial commit

---
+++ sketch.js
@@ -0,0 +1,81 @@
+const circleDm = 25;
+const headNum = 6;
+const yuanGraphics = [];
+let yuanHead;
+let rot = 0;
+let zombo;
+
+function preload() {
+  yuanHead = loadImage("./images/yuan.png");
+  zombo = loadSound("zombo.mp3");
+}
+
+function setup() {
+//YOOOO! You can apply tint() in setup. Storing that new image will give you better performance than applying the tint every frame (that would lag) 
+  for (let i = 0; i < headNum; i++) {
+    let newGraphic = createGraphics(64, 64);
+    newGraphic.colorMode(HSL);
+    newGraphic.tint((360 / headNum) * i, 70, 60);
+    newGraphic.image(yuanHead, 0, 0, 64, 64);
+    yuanGraphics.push(newGraphic);
+  }
+
+  let scaryButton = createButton("ZOMBO 🔊");
+  scaryButton.id("scary-button");
+  scaryButton.mousePressed(zomboPlay);
+  scaryButton.parent('button-container')
+
+  let canvas = createCanvas(400, 400);
+  canvas.parent('p5-container');
+  colorMode(HSL);
+  angleMode(DEGREES);
+  imageMode(CENTER);
+}
+
+function draw() {
+  background(220);
+  push();
+  translate(width / 2, height / 2);
+  image(yuanHead, 0, 0, 64, 64);
+
+  if (rot >= 360) {
+    rot = 0;
+  } else {
+    rot+= 4;
+  }
+  polygon(0, 0, 74, 6, rot);
+  pop();
+
+  // image(yuanHead, mouseX, mouseY, 64, 64);
+}
+
+function polygon(x, y, radius, npoints, rotation = 0) {
+  let angle = 360 / npoints;
+  let i = 0;
+  for (let a = 0 + rotation; a < 360 + rotation; a += angle) {
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
+    image(yuanGraphics[i], sx, sy, 64, 64);
+    i++;
+  }
+}
+
+function zomboPlay() {
+  if (zombo.isPlaying()) {
+    // .isPlaying() returns a boolean
+    zombo.stop();
+    document.querySelector("#scary-button").innerHTML = "ZOMBO";
+  } else {
+    zombo.play();
+    document.querySelector("#scary-button").innerHTML = "Gah, stop!";
+  }
+}
+
+// var headerText = document.getElementsByClassName("title")[0].innerHTML;
+const headerText = document.querySelector('h1').innerText;
+const text = headerText.split("");
+document.querySelector('h1').innerHTML = (text.reduce(rainbowText, ""));
+function rainbowText(result, letter, i, source) {
+  const color = `hsl(${i / source.length * 360}, 100%, 50%)`;
+  return result += `<span style="color: ${color};">${letter}</span>`;
+}
